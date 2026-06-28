17e347925abf6ca221781e10af76535d6b069e0b
f <nafinekouen@gmail.com>
2026-06-28 22:49:40 +0900

    Day 2: find_max関数完成・if文で最大値を探す

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def goukei_list(numbers):
-    total = 0
+def find_max(numbers):
+    max = numbers[0]
     for num in numbers:
-        total = total + num
-    return total
-print(goukei_list([1,2,3,4,5,6,7,8,9]))
+        if num > max:
+            max = num
+    return max
+print(find_max([5,4,5,8,4,1]))
